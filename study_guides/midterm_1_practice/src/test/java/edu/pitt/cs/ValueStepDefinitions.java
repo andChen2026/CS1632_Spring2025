fb4f5cdf1e973fbe18f59a45246baced3580c444
ItsAndrewC! <andew73885@gmail.com>
2025-02-11 13:52:21 -0500

    adding step definitions

--- study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/ValueStepDefinitions.java
+++ study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/ValueStepDefinitions.java
@@ -7,4 +7,13 @@ import static org.junit.Assert.*;
 
 public class ValueStepDefinitions {
 	// TODO: Fill in!
+	@Given("A Value")
+	public void a_value(){
+		// Do Stuff
+	}
+
+    @Given("a Value")
+    public void a_Value() {
+        // Write code here that turns the phrase above into concrete actions
+    }
 }
